caa9c281f34b57abc38ad439dbe59b29bb99c36b
amal <fares.amal@esprit.tn>
2023-05-14 23:05:33 +0200

    jusqua que9/20

---
+++ Examen.ApplicationCore/Services/PassengerExtension.cs
@@ -0,0 +1,18 @@
+﻿using Examen.ApplicationCore.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen.ApplicationCore.Services
+{
+    public static class PassengerExtension
+    {
+            public static void UpperFullName(this Passenger a ) {
+
+                 a.FirstName = a.FirstName[0].ToString().ToUpper()+a.FirstName.Substring(1);
+               a.LastName = a.LastName[0].ToString().ToUpper() + a.LastName.Substring(1);
+             }*/
+    }
+}
