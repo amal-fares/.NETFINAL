cf51e0a443ff0cdea4bebdf5f59d5e0e98bc2957
amal <fares.amal@esprit.tn>
2023-05-14 22:43:23 +0200

    jusqua que8

---
+++ Examen.ApplicationCore/Services/ServiceFlight.cs
@@ -0,0 +1,68 @@
+﻿using Examen.ApplicationCore.Domain;
+using Examen.ApplicationCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen.ApplicationCore.Services
+{
+    public class ServiceFlight : Service<Flight>, IServiceFlight
+    {
+
+        public List<Flight> Flights { get; set; } = new List<Flight>();
+
+        public List<DateTime> GetFlightDates(string destination)
+        {
+            List<DateTime> dates = new List<DateTime>();
+
+            for (int i = 0; i < Flights.Count; i++)
+            {
+                if (Flights[i].Destination == destination)
+                {
+                    dates.Add(Flights[i].FlightDate);
+                }
+            }
+            return dates;
+            ///  //foreach (var flight in Flights)
+            //{
+            //    if (flight.destination == destination)
+            //    {
+            //        dates.Add(flight.flightDate);
+            //    }
+            //}
+            void GetFlights(string filterType, string filterValue)
+            {
+                switch (filterType)
+                {
+                    case "Destination":
+                        foreach (Flight f in Flights)
+                        {
+                            if (f.Destination.Equals(filterValue))
+                                Console.WriteLine(f);
+                        }
+                        break;
+                    case "FlightDate":
+                        foreach (Flight f in Flights)
+                        {
+                            if (f.FlightDate == DateTime.Parse(filterValue))
+
+                                Console.WriteLine(f);
+
+                        }
+                        break;
+                    case "EffectiveArrival":
+                        foreach (Flight f in Flights)
+                        {
+                            if (f.EffectiveArrival == DateTime.Parse(filterValue))
+                                Console.WriteLine(f);
+                        }
+                        break;
+                }
+            }
+
+        }
+    }
+}
+    
